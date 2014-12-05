7bb59018b867dae62c1357ec47b65b92ba11c5e5
zero323 <matthew.szymkiewicz@gmail.com>
2014-12-05 14:31:28 +0100

    Add pubchem_drug_and_medication_information

---
+++ R/pubchem_drug_and_medication_information.R
@@ -0,0 +1,26 @@
+library(jsonlite)
+
+#' Download Drug and Medication Information for a given compound from PubChem
+#'
+#' @param cid
+#' @return list of data.frames 
+#' @export
+
+
+pubchem_drug_and_medication_information <- function(cid) {
+    stopifnot(is.numeric(cid))
+    base_url <- "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/%i/JSON"
+    
+    # Fetch data
+    r <- fromJSON(sprintf(base_url, cid))
+    
+    # Extract record
+    record <- r$Record
+    stopifnot(!is.null(record))
+    
+    # Get index of "Drug and Medication Information" section
+    dmi_index <- which(record$Section$TOCHeading == "Drug and Medication Information")
+    
+    # Extract "Drug and Medication Information" 
+    setNames(apply(d, 1, function(x) do.call(cbind, x[3])), d[, 1])
+}
