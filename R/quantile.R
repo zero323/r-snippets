6f66341a9efd8e84fb3f80c14179649e4cce303b
zero323 <matthew.szymkiewicz@gmail.com>
2015-02-10 01:06:33 +0100

    Add quantile

---
+++ R/quantile.R
@@ -0,0 +1,19 @@
+#' Compute quantiles corresponding to the given probabilities
+#'
+#' @param x numeric vector
+#' @param p numeric vector of probabilites
+#' @return  quantiles of x
+#' @examples
+#' quantile(rnorm(100), 0.5)
+#' quantile(1:100, c(0.25, 0.5, 0.75))
+#' @export
+quantile <- function(x, p) {
+    stopifnot(is.numeric(x), is.finite(x))
+    stopifnot(is.numeric(p), all(p >= 0 && p <= 1))
+    xs <- sort(x)
+    h <- (length(x) - 1) * p + 1
+    q <- xs[floor(h)] + (h - floor(h)) * (xs[floor(h) + 1] - x[floor(h)])
+    # If p == 1 then xs[floor(h) + 1] gives NA hence we have to remove NAs
+    q[is.na(q)] <- xs[length(xs)]
+    q
+}
