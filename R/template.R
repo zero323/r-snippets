44365175b8bc22f85668775628bb7584f932ec23
zero323 <matthew.szymkiewicz@gmail.com>
2015-02-11 17:51:46 +0100

    Add template

---
+++ R/template.R
@@ -0,0 +1,46 @@
+template <- function(dirname, data) {
+    library(stringi)
+    
+    stopifnot(is.character(dirname), length(dirname) == 1)
+    stopifnot(is.data.frame(data), "key" %in% colnames(data), "value" %in% colnames(data))
+    
+    templates <- list.files(dirname, full.names = TRUE, pattern = "\\.tpl$")
+    get_data <- function(template, data) {
+        rbind(
+            data,
+            data.frame(
+                key = c("curtime", "curdate", "filename", "filepath"),
+                value = c(
+                    format(Sys.time(), "%H:%M:%S"),
+                    format(Sys.time(), "%Y-%m-%d"),
+                    basename(template),
+                    template
+                )
+            )
+        )
+    }
+    
+    process_template <- function(template, data) {
+        pattern <- "(?<=%)([a-zA-Z]|\\.[a-zA-Z])[a-zA-Z0-9_\\.]*(?=%)"
+        data <- get_data(template, data)
+        
+        lines <- readLines(template)
+        tags <- Filter(function(x) !is.na(x), unlist(stri_extract_all_regex(lines, pattern)))
+        for(tag in tags) {
+            i <- which(data$key == tag)
+            if (length(i) != 0) {
+                lines <- stri_replace_all_regex(lines, stri_join("%", tag, "%"), data$value[i])
+            } else {
+                warning(paste(tag, "not found"))
+            }
+        }
+        lines
+    }
+    
+    lapply(
+        templates,
+        function(template) writeLines(process_template(template, data), stri_replace_last_regex(template, "tpl$", "txt"))
+    )
+    
+    invisible()
+}
